546eeb25e546c0e26f9d8ef3078863eafb605f9f
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-04-24 18:45:52 +0200

    Little refactor

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
 'use strict'
 
-var asyncify = require('async/asyncify')
 var sliced = require('sliced')
 
 var createProxy = {
   sync: function (fn, handler, errHandler) {
-    var wrapFn = asyncify(fn)
-
     function proxy () {
-      var proxyArgs = sliced(arguments)
-
-      function invoke () {
-        var args = sliced(arguments)
-        var err = args.shift()
-
-        if (err) {
-          if (errHandler) {
-            errHandler(err)
-            throw err
-          }
+      try {
+        fn.apply(fn, arguments)
+        handler.apply(handler, arguments)
+      } catch (err) {
+        if (errHandler) {
+          errHandler(err)
+          throw err
         }
-
-        handler.apply(handler, args)
       }
-
-      proxyArgs.push(invoke)
-      wrapFn.apply(wrapFn, proxyArgs)
     }
     return proxy
   },
